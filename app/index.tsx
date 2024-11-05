c250609e358b4091d06fe96512d04f7ee5ab1b72
Krishna Tiwari <61178259+krishna-cyber@users.noreply.github.com>
2024-11-05 16:19:02 +0545

    index page designing

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Link } from "expo-router";
@@ -7,36 +7,36 @@ import { SafeAreaView } from "react-native-safe-area-context";
 const Index = () => {
   return (
     <SafeAreaView className="h-full bg-primary">
-      <ScrollView contentContainerStyle={{height:'100%'}}>
-    <View className=" flex justify-center items-center h-screen">
-      <Text className=" text-red-500 font-PoppinsBold text-lg font-medium border-blue-500 border-dashed border-4 mt-4 p-4 w-fit ">
-        Index
-      </Text>
-      <StatusBar style="auto" />
-      <Link
-        href="/profile"
-        style={{ color: "blue", backgroundColor: "red" }}
-        className=" font-PoppinsBlack"
-      >
-        Go to profile
-      </Link>
-      <Link
-        href="/(tabs)/home"
-        style={{ color: "blue", backgroundColor: "red" }}
-        className=" font-PoppinsBlack"
-      >
-        Go to Home
-      </Link>
-      <Link
-        href="/(tabs)/create"
-        style={{ color: "blue", backgroundColor: "red" }}
-        className=" font-PoppinsBlack"
-      >
-        Go to create
-      </Link>
-    </View>
-
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className=" w-full h-[85vh] px-4 justify-center  items-center">
+          <Image
+            source={require("../assets/images/logo.png")}
+            className=" max-w-36 max-h-36"
+            resizeMode="contain"
+          />
+          <Image
+            source={require("../assets/images/cards.png")}
+            className=" max-w-[80%] max-h-[30%]"
+            resizeMode="contain"
+          />
+          <View className=" relative mt-5">
+            <Text className="text-3xl text-center text-white font-bold">
+              Discover endless possibilities with {""}
+              <Text className=" text-secondary">Aora</Text>
+            </Text>
+            <Image
+              source={require("../assets/images/path.png")}
+              className="absolute h-4 w-20 left-[70%] top-[62px]"
+              resizeMode="contain"
+            />
+            <Text className=" text-gray-200 mt-3 text-sm font-pregular text-center">
+              Where creativity meets expectation: embark on a journey of
+              limitless possibilities with Aora.
+            </Text>
+          </View>
+        </View>
       </ScrollView>
+      <StatusBar backgroundColor="#010515" translucent={true} style="light" />
     </SafeAreaView>
   );
 };
